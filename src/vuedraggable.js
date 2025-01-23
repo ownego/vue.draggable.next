e3e5b79fc6d5833c78571c32b52e90a249458131
TinhPT <tinh.phantrong0612@gmail.com>
2025-01-23 17:11:37 +0700

    Deselect after drag end

--- src/vuedraggable.js
+++ src/vuedraggable.js
@@ -510,7 +510,9 @@ const draggableComponent = defineComponent({
       return move(sendEvent, originalEvent);
     },
 
-    onDragEnd() {
+    onDragEnd(evt) {
+      const fallback = () => {};
+      Array.isArray(evt?.items) && evt.items.forEach(Sortable?.utils?.deselect ?? fallback);
       draggingElement = null;
     }
   }
